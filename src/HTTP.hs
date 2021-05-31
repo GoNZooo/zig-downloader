01e4ccc93080e723c495f54758e25e8ec905c9f2
Rickard Andersson <gonz@severnatazvezda.com>
2021-05-31 07:39:25 +0000

    add helper for downloading JSON->type

--- src/HTTP.hs
+++ src/HTTP.hs
@@ -1,12 +1,24 @@
 module HTTP where
 
+import Data.Aeson (FromJSON, eitherDecode')
 import Network.HTTP.Client
 import RIO
+import qualified RIO.ByteString.Lazy as LazyByteString
 import Types
 
+downloadJSON :: (FromJSON a) => Url -> RIO App (Either String a)
+downloadJSON url = do
+  data' <- downloadData url
+  data' & eitherDecode' & pure
+
 downloadBinaryFile :: Url -> FilePath -> RIO App ()
-downloadBinaryFile (Url url) path = do
+downloadBinaryFile url path = do
+  data' <- downloadData url
+  writeFileBinary path $ toStrictBytes data'
+
+downloadData :: Url -> RIO App LazyByteString.ByteString
+downloadData (Url url) = do
   manager <- asks appTlsManager
   request <- liftIO $ parseRequest url
   response <- liftIO $ httpLbs request manager
-  writeFileBinary path $ toStrictBytes $ responseBody response
+  pure $ responseBody response
--- src/ZigIndex.hs
+++ src/ZigIndex.hs
@@ -1,20 +1,14 @@
 module ZigIndex where
 
-import qualified Data.Aeson as JSON
-import Network.HTTP.Client
+import HTTP
 import RIO
 import Types
 
-indexUrl :: String
-indexUrl = "https://ziglang.org/download/index.json"
+indexUrl :: Url
+indexUrl = Url "https://ziglang.org/download/index.json"
 
 getVersions :: RIO App (Either String Versions)
-getVersions = do
-  manager <- asks appTlsManager
-  request <- liftIO $ parseRequest indexUrl
-  response <- liftIO $ httpLbs request manager
-
-  pure $ JSON.eitherDecode' $ responseBody response
+getVersions = downloadJSON indexUrl
 
 getMaster :: RIO App (Either String Master)
 getMaster = fmap master <$> getVersions
